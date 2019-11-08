1a9620da9c5d8ef0c0378b28eb0e0cabd4316aab
ChaoY000000 <chaoyongpeng@163.com>
2019-11-08 11:42:11 +0800

    测试 push

---
+++ src/main/java/com/chao/CyNoteApplication.java
@@ -0,0 +1,13 @@
+package com.chao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyNoteApplication.class, args);
+	}
+
+}
