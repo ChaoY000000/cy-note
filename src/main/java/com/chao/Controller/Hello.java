43555add1e4b1dab3eacb33df5f7ba06535e1603
ChaoY000000 <chaoyongpeng@163.com>
2019-11-08 11:11:47 +0800

    XXXX

--- src/main/java/com/chao/Controller/Hello.java
+++ src/main/java/com/chao/Controller/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("调试git环境不能在idea push问题");
+        System.out.println("调试git环境不能在idea push问题 再试");
     }
 }
